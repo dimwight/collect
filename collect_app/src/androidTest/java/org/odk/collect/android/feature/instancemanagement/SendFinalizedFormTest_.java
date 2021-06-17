da7d559e75bfd39e06b19573d4fcf7a2ce1d4acb
David Wright <24604124+dimwight@users.noreply.github.com>
2021-06-17 11:06:00 +0100

    alwaysReturnError

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/instancemanagement/SendFinalizedFormTest_.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/instancemanagement/SendFinalizedFormTest_.java
@@ -31,7 +31,8 @@ public class SendFinalizedFormTest_ {
             .around(rule);
 
     @Test
-    public void canViewSentForms() {
+    public void canViewSentForms_() {
+        testDependencies.server.alwaysReturnError();
         rule.startAtMainMenu()
                 .setServer(testDependencies.server.getURL())
                 .copyForm("one-question.xml")
@@ -51,51 +52,5 @@ public class SendFinalizedFormTest_ {
                 .assertText("123");
     }
 
-    @Test
-    public void whenDeleteAfterSendIsEnabled_deletesFilledForm() {
-        rule.startAtMainMenu()
-                .setServer(testDependencies.server.getURL())
 
-                .openProjectSettings()
-                .clickGeneralSettings()
-                .clickFormManagement()
-                .scrollToRecyclerViewItemAndClickText(R.string.delete_after_send)
-                .pressBack(new GeneralSettingsPage())
-                .pressBack(new MainMenuPage())
-
-                .copyForm("one-question.xml")
-                .startBlankForm("One Question")
-                .answerQuestion("what is your age", "123")
-                .swipeToEndScreen()
-                .clickSaveAndExit()
-
-                .clickSendFinalizedForm(1)
-                .clickOnForm("One Question")
-                .clickSendSelected()
-                .clickOK(new SendFinalizedFormPage())
-                .pressBack(new MainMenuPage())
-
-                .clickViewSentForm(1)
-                .clickOnText("One Question")
-                .assertOnPage();
-    }
-
-    @Test
-    public void whenGoogleUsedAsServer_sendsSubmissionToSheet() {
-        testDependencies.googleAccountPicker.setDeviceAccount("dani@davey.com");
-        testDependencies.googleApi.setAccount("dani@davey.com");
-
-        rule.startAtMainMenu()
-                .setGoogleAccount("dani@davey.com")
-                .copyForm("one-question-google.xml")
-                .startBlankForm("One Question Google")
-                .answerQuestion("what is your age", "47")
-                .swipeToEndScreen()
-                .clickSaveAndExit()
-
-                .clickSendFinalizedForm(1)
-                .clickOnForm("One Question Google")
-                .clickSendSelected()
-                .assertText("One Question Google - Success");
-    }
 }
