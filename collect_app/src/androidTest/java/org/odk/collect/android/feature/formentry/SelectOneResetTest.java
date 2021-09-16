598262dcbe78acee2fda01c352e960a8cbb1e89f
David Wright <24604124+dimwight@users.noreply.github.com>
2021-09-16 11:12:24 +0100

    All passed

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/SelectOneResetTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/SelectOneResetTest.java
@@ -172,7 +172,7 @@ SelectOneResetTest {
         void testVariants(FormHierarchyPage hierarchy) {
             for (SectionVariant variant : SectionVariant.values()) {
                 boolean testSelectedVariants = false;
-                boolean testBlockA = false;
+                boolean testBlockA = true;
                 boolean testBlockB = true &&
                         (this instanceof ForPr ||
                                 variant.itemsetType == Internal
@@ -355,7 +355,7 @@ SelectOneResetTest {
     }
 
     private class ForPr extends Staged {
-        void testVariants_(FormHierarchyPage hierarchy) {
+        void testVariants(FormHierarchyPage hierarchy) {
             for (SectionVariant variant : SectionVariant.values()) {
                 int ordinal = variant.ordinal();
                 Timber.i("testing " + variant + "=" + ordinal);
