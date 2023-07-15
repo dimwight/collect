87d485134eb93f1e97e1650417becf607938d190
dimwight <dimwight@gmail.com>
2023-07-15 19:26:34 +0100

    For PR

--- collect_app/src/main/java/org/odk/collect/android/formentry/ODKView.java
+++ collect_app/src/main/java/org/odk/collect/android/formentry/ODKView.java
@@ -473,14 +473,14 @@ public class ODKView extends SwipeHandler.View implements OnLongClickListener, W
         });
     }
 
-    //For 3027
+    //For #3027
     public void setFocus(Context context) {
         if (widgets.isEmpty()) {
             return;
         }
         int activeAt = 0;
         if (STAGE_1.isLive()) {
-            //Retrieve and ?clear marker, set active #3027
+            //Retrieve and ?clear marker, set active
             FormIndex activeIndex = FormHierarchyActivity.getActiveIndex(true);
             for (int at = 0; at < widgets.size(); at++) {
                 //Only set index >=0 if match found
