f1df55c3854693cf6b92e425e99abe5fa81107fd
dimwight <dimwight@gmail.com>
2023-07-31 12:47:31 +0100

    FAILURE_5358

--- collect_app/src/main/java/org/odk/collect/android/utilities/FormsDownloadResultInterpreter.kt
+++ collect_app/src/main/java/org/odk/collect/android/utilities/FormsDownloadResultInterpreter.kt
@@ -1,7 +1,6 @@
 package org.odk.collect.android.utilities
 
 import android.content.Context
-import org.odk.collect.android.R
 import org.odk.collect.android.formmanagement.FormDownloadException
 import org.odk.collect.android.formmanagement.FormDownloadExceptionMapper
 import org.odk.collect.android.formmanagement.ServerFormDetails
@@ -9,21 +8,41 @@ import org.odk.collect.errors.ErrorItem
 import org.odk.collect.strings.localization.getLocalizedString
 
 object FormsDownloadResultInterpreter {
+    private const val FAILURE_5358 = false
     fun getFailures(result: Map<ServerFormDetails, FormDownloadException?>, context: Context) = result.filter {
-        it.value != null
+        FAILURE_5358 || it.value != null
     }.map {
-        ErrorItem(
-            it.key.formName ?: "",
-            context.getLocalizedString(org.odk.collect.strings.R.string.form_details, it.key.formId ?: "", it.key.formVersion ?: ""),
-            FormDownloadExceptionMapper(context).getMessage(it.value)
-        )
+        val errorItem = if (FAILURE_5358) {
+            ErrorItem(
+                "formName",
+                context.getLocalizedString(
+                    org.odk.collect.strings.R.string.form_details,
+                    "formId",
+                    "formVersion"
+                ),
+                "getMessage"
+            )
+        } else
+            ErrorItem(
+                it.key.formName ?: "",
+                context.getLocalizedString(
+                    org.odk.collect.strings.R.string.form_details,
+                    it.key.formId ?: "",
+                    it.key.formVersion ?: ""
+                ),
+                FormDownloadExceptionMapper(context).getMessage(it.value)
+            )
+        errorItem
     }
 
     fun getNumberOfFailures(result: Map<ServerFormDetails, FormDownloadException?>) = result.count {
-        it.value != null
+        FAILURE_5358 || it.value != null
     }
 
-    fun allFormsDownloadedSuccessfully(result: Map<ServerFormDetails, FormDownloadException?>) = result.values.all {
-        it == null
+    fun allFormsDownloadedSuccessfully(result: Map<ServerFormDetails, FormDownloadException?>): Boolean {
+        return !FAILURE_5358 &&
+                result.values.all {
+                    it == null
+                }
     }
 }
