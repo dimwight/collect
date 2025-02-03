fab9f5e31e30ca1d74fa8ea501e1c84124cdc849
dimwight <dimwight@gmail.com>
2025-02-03 18:44:12 +0000

    GregorianDatePickerDialog

---
+++ collect_app/src/main/java/org/odk/collect/android/widgets/datetime/pickers/GregorianDatePickerDialog.java
@@ -0,0 +1,18 @@
+package org.odk.collect.android.widgets.datetime.pickers;
+
+import org.joda.time.LocalDateTime;
+
+public class GregorianDatePickerDialog extends CustomDatePickerDialog {
+    public GregorianDatePickerDialog() {
+    }
+
+    @Override
+    protected void updateDays() {
+
+    }
+
+    @Override
+    protected LocalDateTime getOriginalDate() {
+        return null;
+    }
+}
